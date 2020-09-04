42ced57b312cefc1649b115cf1fb7c17416aef46
nickypangers <nixonpang98@gmail.com>
2020-09-05 02:05:04 +0800

    Updates

---
+++ precache-manifest.8e6a08cbd82a732825bcfb5e669daf4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bcdb017669bc2371356ef7523fff06a",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "dac566f286eadbc02777",
+    "url": "/simple-todo-app/static/css/main.80130322.chunk.css"
+  },
+  {
+    "revision": "04821c5a983e53d8d82c",
+    "url": "/simple-todo-app/static/js/2.d39b50b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.d39b50b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac566f286eadbc02777",
+    "url": "/simple-todo-app/static/js/main.2b4459e8.chunk.js"
+  },
+  {
+    "revision": "04cda6b01e67aca66d7f",
+    "url": "/simple-todo-app/static/js/runtime-main.7241a5de.js"
+  }
+]);
